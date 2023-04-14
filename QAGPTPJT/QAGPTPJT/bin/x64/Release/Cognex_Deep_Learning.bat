772aee5886324057d272ee3128ef44a0ff306111
Jongkwan Park <Jongkwan.Park@cognex.com>
2023-04-14 11:25:48 +0900

    rebuild and adding file

---
+++ QAGPTPJT/QAGPTPJT/bin/x64/Release/Cognex_Deep_Learning.bat
@@ -0,0 +1,12 @@
+::Cognex Deep Learning
+::Current Path
+set CurPath=%cd%
+
+::Set Env Parameter
+set PATH=%PATH%;%CurPath%\bin
+set VPDL_BIN_V30=%CurPath%\bin
+set VPDL_ROOT_V30=%CurPath%
+
+::Run File
+::"%CurPath%\Cognex Deep Learning Studio\Cognex Deep Learning Studio.exe"
+"%CurPath%\QAGPTPJT.exe"
