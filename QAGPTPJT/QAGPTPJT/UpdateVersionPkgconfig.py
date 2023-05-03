688931d8fea72fc67fc9778cc57af651adca318d
Jongkwan Park <Jongkwan.Park@cognex.com>
2023-05-03 18:04:41 +0900

    Adding python file which is final version for update version

--- QAGPTPJT/QAGPTPJT/UpdateVersionPkgconfig.py
+++ QAGPTPJT/QAGPTPJT/UpdateVersionPkgconfig.py
@@ -1,14 +1,17 @@
 import os
 import re
+import argparse # Step 1. import argparse
+
 print('Start : Modify ViDi packages version to use Artifact dependencies in teamcity')
-print(os.path.abspath(__file__)) # Get the current directory
-## %Workspace%%OS.Path.Separator%QAGPTPJT\QAGPTPJT\packages.config
-#pathPKG = '%Workspace%%OS.Path.Separator%QAGPTPJT\QAGPTPJT\packages.config'
+print('Get the current directory of python file: ' + os.path.abspath(__file__)) # Get the current directory of python file.
 
-#pathPKG = 'H:\\Test_PythonVPDLVersion\\packages.config'
-pathPKG = 'packages.config'
+parser = argparse.ArgumentParser() # Step 2. Create parser
+parser.add_argument("-v", "--version", default="7.0.0.00000") # Step 3. Register parameter to be got by parser.add_argment()
+args = parser.parse_args() # Step 4. Analyze parameters
+print('-v=' + args.version)
+appliedVersion = args.version
 
-appliedVersion = "2.2.2.22222"
+pathPKG = 'packages.config' ## %Workspace%%OS.Path.Separator%QAGPTPJT\QAGPTPJT\packages.config
 
 f = open(pathPKG, 'r')
 lines = f.readlines()
@@ -17,14 +20,17 @@ allLines = []
 for line in lines:
     allLines.append(line)            
 f.close()
+
 newAllLines = []
 for content in allLines:
     if(content.find('ViDi')>0 and content.find('version=')>0):                    
         newAllLines.append(re.sub(content[content.find('version=')+9:content.find('version=')+20], appliedVersion, content))        
     else:
         newAllLines.append(content)
+
 f = open(pathPKG, 'w')
 for str in newAllLines:
     f.writelines(str)
 f.close()
-print('End : Complete update ViDi version in  packages.config')
+
+print('End : Complete update ViDi version in packages.config')
